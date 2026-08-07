5cb69ae0a8da11a16264946c351b911bffa6e22c
‪Ahmed Mabrouk‬‏ <mabrouk@usa.com>
2026-08-07 06:03:03 +0400

    Delete healthedia directory

--- healthedia/includes/class-healthedia-deactivator.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Healthedia_Deactivator {
-	public static function deactivate() {
-		flush_rewrite_rules();
-	}
-}
--- healthedia/modules/Notifications/class-notification-controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Healthedia_Notification_Controller extends Healthedia_Base_Controller {
-    public function init( $loader ) {
-        require_once HEALTHEDIA_PLUGIN_DIR . 'modules/Notifications/class-notification-api.php';
-        $api = new Healthedia_Notification_API();
-        $loader->add_action( 'rest_api_init', $api, 'register_routes' );
-    }
-}
